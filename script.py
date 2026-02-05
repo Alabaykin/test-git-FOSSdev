c46869300e2168872055e138a7986a163edf7233
ArkadiyBell <bells.vtv@gmail.com>
2026-02-05 14:55:41 +0800

    Added check for divide

--- script.py
+++ script.py
@@ -1,7 +1,10 @@
-def sun(a, b):
+def sum(a, b):
     return a + b
+
 def devide(a, b):
     if b  == 0:
         raise ValueError("Determinator could not be zero")
+    if isinstance(a, str) or isinstance(b, str):
+        raise ValueError("Could not divide strings")
     return a / b
 
