22afbc1a4d74caaf25d39a8b6b058aeb618e5ba6
ArkadiyBell <bells.vtv@gmail.com>
2026-03-05 14:54:15 +0800

    Initial commit

---
+++ testing/frameworks/tests/tax/test_income.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append("../src")
+from tax.income import calculate_tax
+
+def test_icome_tax():
+
+    assert calculate_tax(100) == 15
+    print("TEST TAX PASSED")
+
+if __name__ == "__main__":
+    test_icome_tax()
