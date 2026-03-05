34361efe5a752b3199a487d5dfb0a9fe249a937b
ArkadiyBell <bells.vtv@gmail.com>
2026-03-05 12:31:08 +0800

    Initial commit for testing practice. Added test for addition

---
+++ testing/principle/tests/test_principles.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append("../src")
+from math_demo import add
+
+def test_addition():
+    assert 2 + 2 == 4
+    print("TEST ADDITION PASSED")
+
+if __name__ == "__main__":
+    test_addition()
