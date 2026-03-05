9d4cf6e707d931a4f6ae030aa5cbda2046ecfbb5
ArkadiyBell <bells.vtv@gmail.com>
2026-03-05 14:19:49 +0800

    Added tax tests

--- testing/principle/tests/test_principles.py
+++ testing/principle/tests/test_principles.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append("../src")
-from math_demo import add, add_with_bug
+from math_demo import add, add_with_bug, calculate_tax_bugged, calculate_tax
 
 def test_addition():
     assert add(2, 2) == 4
@@ -16,10 +16,32 @@ def test_addition_with_bug():
     print("TEST BUGGED ADDITION PASSED")
 
 def test_addition_dublicate():
-    assert add(2, 2) == 6 + 7
+    assert add(6, 7) == 6 + 7
     print("TEST DUBLICATE ADDITION PASSED")
+
+def test_tax_calculator_pesticide():
+    assert calculate_tax_bugged(1000) == 150
+    assert calculate_tax_bugged(100) == 15
+    assert calculate_tax_bugged(10) == 1.5
+    assert calculate_tax_bugged(1) == 0.15
+    assert calculate_tax_bugged(234) == 35.1
+    print("TEST TAX CALCULATOR PASSED")
+    # float may give us test cases
+    #assert calculate_tax_bugged(2.34) == 0.35
+
+def test_tax_calculator():
+    assert calculate_tax(1000) == 150
+    assert calculate_tax(100) == 15
+    assert calculate_tax(10) == 1.5
+    assert calculate_tax(1) == 0.15
+    assert calculate_tax(234) == 35.1
+    assert calculate_tax(2.34) == 0.35
+    print("TEST TAX CALCULATOR PASSED")
+
 
 if __name__ == "__main__":
     test_addition()
     test_addition_with_bug()
     test_addition_dublicate()
+    test_tax_calculator_pesticide()
+    test_tax_calculator()
