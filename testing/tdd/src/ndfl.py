9db17209b67370f28ddd4014f5bb76d5e9b63f2d
ArkadiyBell <bells.vtv@gmail.com>
2026-03-10 14:52:56 +0800

    Made all test for basic_ndfl

--- testing/tdd/src/ndfl.py
+++ testing/tdd/src/ndfl.py
@@ -1,9 +1,38 @@
 def calculate_ndfl(income: int | float) -> int | float:
     result: int | float = 0
-    if income <= 2_400_000:
+    
+    if income < 2_400_000:
         result = income * 0.13
-    else:
-        result = 2400000*0.13 + (income - 2400000) * 0.15
-    9_402_000 + (income - 50_000_000) * 0.22
         
+    elif income < 5_000_000:
+        result = (
+            2_400_000 * 0.13 +
+            (income - 2_400_000) * 0.15
+        )
+    
+    elif income < 20_000_000:
+        result = (
+            2_400_000 * 0.13 +
+            2_600_000 * 0.15 +
+            (income - 5_000_000) * 0.18
+        )
+    
+    elif income < 50_000_000:
+        result = (
+            2_400_000 * 0.13 +
+            2_600_000 * 0.15 +
+            15_000_000 * 0.18 +
+            (income - 20_000_000) * 0.20
+        )
+    
+    else:
+        result = (
+            2_400_000 * 0.13 +
+            2_600_000 * 0.15 +
+            15_000_000 * 0.18 +
+            30_000_000 * 0.20 +
+            (income - 50_000_000) * 0.22
+        )
+    
     return result
+
