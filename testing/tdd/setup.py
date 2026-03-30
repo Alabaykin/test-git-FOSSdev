ea53b12aef1db82590bd80687c9a348396ed34ee
ArkadiyBell <bells.vtv@gmail.com>
2026-03-30 22:04:16 +0800

    Before checkout

--- testing/tdd/setup.py
+++ testing/tdd/setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup, find_packages
 setup(
-    name = "income_tax",
+    name = "income_tax_pomidor",
     version = "0.0.0",
     package_dir={"": "src"},
-    packages=find_packages(where="src")
+    packages=find_packages(where="src"),
+    author="Alabaykin",
+    author_email="bells.vtv@gmail.com",
+    url="https://github.com/Alabaykin/test-git-FOSSdev/tree/feature/testing/testing/tdd"
 )
